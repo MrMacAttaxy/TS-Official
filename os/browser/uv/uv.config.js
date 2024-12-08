a9a9d5c3c8ed5ab31f44830573db4832a8db00c7
MrMacAttaxy <116235140+MrMacAttaxy@users.noreply.github.com>
2024-12-07 23:04:43 -0500

    Update and rename os/browser/uv.config.js to os/browser/uv/uv.config.js

---
+++ os/browser/uv/uv.config.js
@@ -0,0 +1,10 @@
+self.__uv$config = {
+    prefix: '/os/browser/',
+    bare:'https://bare.benrogo.net',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/os/browser/uv/uv.handler.js',
+    bundle: '/os/browser/uv/uv.bundle.js',
+    config: '/os/browser/uv/uv.config.js',
+    sw: '/os/browser/uv/uv.sw.js',
+};
